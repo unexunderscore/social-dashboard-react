f1f6047f8708ad8609c0e97350256c90b3d63e37
Attila Tóth <toth_attila@outlook.hu>
2022-03-22 07:30:05 +0100

    Social big cards layout on mobile 'done', overview section in progress

--- src/Components/Bigcards/Bigcard.jsx
+++ src/Components/Bigcards/Bigcard.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Bigcard.css';
+import faceIcon from '../../../src/icon-facebook.svg';
+import arrowUp from '../../../src/icon-up.svg';
+import arrowDown from '../../../src/icon-down.svg';
+import twitterIcon from '../../../src/icon-twitter.svg';
+import instaIcon from '../../../src/icon-instagram.svg';
+import youtubeIcon from '../../../src/icon-youtube.svg';
 
 
 
@@ -8,34 +14,62 @@ const Bigcard = () => {
     <div className='big-cards'>
         <div className='big-cardlayout facebook-thame'>
 
-            <div>@nathanf</div>
-            <div>1987</div>
-            <div>Followers</div>
-            <div>12 Today</div>
-                    
+            <div className='iconName-container'>
+                <div><img src={faceIcon} alt="facebook icon" /></div>
+                <div className='social-name'>@nathanf</div>
+            </div>
+            <div className='followers-counter'>1987</div>
+            <div className='follow-title'>Followers</div>
+            <div className='iconToday-container'>
+                <div><img src={arrowUp} alt="Up arrow" /></div>
+                <div className='counterToday greenCounter'>12 Today</div>
+            </div>        
          </div>
+         
         <div className='big-cardlayout twitter-thame'>
 
-            <div>@nathanf</div>
-            <div>1044</div>
-            <div>Followers</div>
-            <div>99 Today</div>
+            <div className='iconName-container'>
+                <div><img src={twitterIcon} alt="twitter icon" /></div>
+                <div className='social-name'>@nathanf</div>
+            </div>
+            <div className='followers-counter'>1044</div>
+            <div className='follow-title'>Followers</div>
+            <div className='iconToday-container'>
+                <div><img src={arrowUp} alt="Up arrow" /></div>
+                <div className='counterToday greenCounter'>99 Today</div>
+            </div> 
+           
 
         </div>
         <div className='big-cardlayout insta-thame'>
 
-            <div>@realnathanf</div>
-            <div>11k</div>
-            <div>Followers</div>
-            <div>1099 Today</div>
+            <div className='iconName-container'>
+                <div><img src={instaIcon} alt="insta icon" /></div>
+                <div className='social-name'>@realnathanf</div>
+            </div>
+            <div className='followers-counter'>11k</div>
+            <div className='follow-title'>Followers</div>
+            <div className='iconToday-container'>
+                <div><img src={arrowUp} alt="Up arrow" /></div>
+                <div className='counterToday greenCounter'>1099 Today</div>
+            </div> 
+            
 
         </div>
         <div className='big-cardlayout youtube-thame'>
 
-            <div>Nathan F.</div>
-            <div>8239</div>
-            <div>Subscribers</div>
-            <div>144 Today</div>
+            <div className='iconName-container'>
+                <div><img src={youtubeIcon} alt="youtube icon" /></div>
+                <div className='social-name'>Nathan F.</div>
+            </div>    
+            <div className='followers-counter'>8239</div>
+            <div className='follow-title'> Subscribers</div>
+            <div className='iconToday-container'>
+                <div><img src={arrowDown} alt="Down arrow" /></div>
+                <div className='counterToday redCounter'>144 Today</div>
+            </div> 
+            
+           
 
         </div>
        
